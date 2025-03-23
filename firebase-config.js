d199576d7f7bfd6fbbf2cf768ddafc2ce023319b
louiek05 <louie0515a@gmail.com>
2025-03-23 11:35:07 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,20 +1,18 @@
-// Firebase 配置
-const firebaseConfig = {
-  apiKey: "AIzaSyAkCEHAwG2pbPGbRXVPutzXD43FcmAG-7I",
-  authDomain: "adventure-card-project.firebaseapp.com",
-  projectId: "adventure-card-project",
-  storageBucket: "adventure-card-project.appspot.com",
-  messagingSenderId: "850675878938",
-  appId: "1:850675878938:web:2436d974f2d0513e6ed5a3"
-};
+// 確保只初始化一次
+if (typeof window.myFirebaseApp === 'undefined') {
+    const config = {
+        apiKey: "AIzaSyAkCEHAwG2pbPGbRXVPutzXD43FcmAG-7I",
+        authDomain: "adventure-card-project.firebaseapp.com",
+        projectId: "adventure-card-project",
+        storageBucket: "adventure-card-project.appspot.com",
+        messagingSenderId: "850675878938",
+        appId: "1:850675878938:web:2436d974f2d0513e6ed5a3"
+    };
 
-// 初始化 Firebase
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
+    // 初始化 Firebase
+    window.myFirebaseApp = firebase.initializeApp(config);
 }
 
-// 初始化 Firestore
-const db = firebase.firestore();
-
-// 初始化 Auth
-const auth = firebase.auth();
+// 確保這些變數可以全局訪問
+window.db = firebase.firestore();
+window.auth = firebase.auth();
